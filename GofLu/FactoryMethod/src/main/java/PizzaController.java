63c6cbf5467453bac3d24a4d3d129e5810a67684
tushgee <tushgee@gmail.com>
2019-07-26 13:37:25 -0500

    Factory Method Demo

--- GofLu/FactoryMethod/src/main/java/PizzaController.java
+++ GofLu/FactoryMethod/src/main/java/PizzaController.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-public class PizzaController {
+@WebServlet("/pizza")
+public class PizzaController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("Your order is ready.");
+    }
 }
