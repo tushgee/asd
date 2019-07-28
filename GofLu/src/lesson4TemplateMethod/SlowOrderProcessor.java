124f4643256cae0a33f5468c995b7ac109c36c23
tushgee <tushgee@gmail.com>
2019-07-28 11:07:05 -0500

    Concrete Methods

--- GofLu/src/lesson4TemplateMethod/SlowOrderProcessor.java
+++ GofLu/src/lesson4TemplateMethod/SlowOrderProcessor.java
@@ -1,4 +1,20 @@
 package lesson4TemplateMethod;
 
-public class SlowOrderProcessor {
+public class SlowOrderProcessor extends AOrderProcessor {
+    @Override
+    public float getShippingFee(IOrder order) {
+        // calculate shipping cost based on order details
+        return 999;
+    }
+
+    @Override
+    public int getShippingTime(IOrder order) {
+        // get shipping time based on shipping address
+        return 60;
+    }
+
+    @Override
+    public void sendConfirmation(String orderDetails, float shippingFee, int shippingTime) {
+        // send by email
+    }
 }
